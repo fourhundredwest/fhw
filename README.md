c091f5e1b3b7d282940c3ed95e20b59e2d47dcfd
mvllow <mvllow@icloud.com>
2024-04-16 14:31:23 -0500

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# fhw
+
+> The website for FourHundredWest
+
+## Setup
+
+Install the dependencies:
+
+```bash
+pnpm install
+```
+
+## Development
+
+Start the development server on `http://localhost:3000`:
+
+```bash
+pnpm dev
+```
+
+## Production
+
+Build the application for production:
+
+```bash
+pnpm build
+```
+
+Locally preview production build:
+
+```bash
+pnpm preview
+```
---
+++ app.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+useHead({
+	htmlAttrs: {
+		lang: "en",
+	},
+});
+useSeoMeta({
+	titleTemplate: "FourHundredWest - %s",
+	description:
+		"FourHundredWest is a development agency that specializes in building websites and web applications.",
+	ogImage: "/og-image.png",
+	ogImageAlt: "FourHundredWest",
+});
+</script>
+
+<template>
+	<a href="#content" class="sr-only focus:not-sr-only">Skip to main content</a>
+
+	<header>
+		<nav aria-label="Primary">
+			<NuxtLink to="/">FourHundredWest</NuxtLink>
+		</nav>
+	</header>
+
+	<main id="content">
+		<NuxtPage />
+	</main>
+</template>

d657090787734b1d5a9a261bd05e84cf7ff9a534
mvllow <mvllow@icloud.com>
2024-04-17 10:39:26 -0500

    add google fonts module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,17 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 	css: ["~/assets/css/main.css"],
+	modules: [
+		[
+			// https://google-fonts.nuxtjs.org
+			"@nuxtjs/google-fonts",
+			{
+				families: {
+					"DM Sans": [400, 500, 600, 700],
+				},
+			},
+		],
+	],
 	postcss: {
 		plugins: {
 			"tailwindcss/nesting": {},

64a76947e25858e1a7be57fa5b182cef6cbb0b48
mvllow <mvllow@icloud.com>
2024-04-19 15:23:22 -0500

    add eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 	css: ["~/assets/css/main.css"],
 	modules: [
+		"@nuxt/eslint",
 		[
 			// https://google-fonts.nuxtjs.org
 			"@nuxtjs/google-fonts",
